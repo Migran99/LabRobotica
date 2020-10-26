d4a2dc14bb68cf3787774c8a7f385570bb794867
David Tejero <davidtejero1999@gmail.com>
2020-10-26 11:49:46 +0100

    Corrijo algún bug

--- TEST_RobotMovil/TEST_RobotMovil.ino
+++ TEST_RobotMovil/TEST_RobotMovil.ino
@@ -55,9 +55,10 @@ void BTread() {
   int dataIn;
   int mod;
   mod = Serial2.read();
-
+  Serial.println(mod);
   while (Serial2.available() > 0) {
     dataIn = int(Serial2.read());
+    Serial.println(dataIn);
     if (dataIn >= 48 && dataIn <= 57) SerialIn = 10 * SerialIn + dataIn - 48;
   }
 
@@ -71,7 +72,7 @@ void BTread() {
       Serial.println("END"); //Debug
       break;
     case 's': //Set distStop
-      distStop = dataIn;
+      distStop = SerialIn;
       Serial.println("Max Distance changed to -->  " + String(distStop)); //Debug
       break;
   }
